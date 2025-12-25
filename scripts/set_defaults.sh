84d573277c5dde358fc22ae68d6f0867541a7512
weixiang <yweixiang2000@gmail.com>
2025-12-26 01:50:53 +0800

    feat(scripts): set git config in script

--- scripts/set_defaults.sh
+++ scripts/set_defaults.sh
@@ -5,3 +5,14 @@ chsh -s $(which zsh)
 
 # use zen
 xdg-settings set default-web-browser app.zen_browser.zen.desktop
+
+# git stuff
+echo "Git Configurations"
+echo "------------------"
+echo "Enter Name:"
+read name
+git config --global user.name $name
+echo "Enter Email:"
+read email
+git config --global user.email $email
+git config --global core.editor nvim
