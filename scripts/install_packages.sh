c18dc1f02bd7d3a78946aa5beb1d44bc8079cb72
weixiang <yweixiang2000@gmail.com>
2025-12-30 13:56:17 +0800

    chore: remove snap (dislike snap in $HOME)

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -5,7 +5,6 @@ set -euo pipefail
 PACKAGES=(
 	zsh
 	neovim
-	snapd
 	stow
 )
 
@@ -14,14 +13,9 @@ FLATPAK=(
 	md.obsidian.Obsidian
 	com.bitwarden.desktop
 	app.zen_browser.zen
-)
-
-SNAP=(
-	youtube-music-desktop-app
+	app.ytmdesktop.ytmdesktop
 )
 
 sudo dnf install -y ${PACKAGES[@]}
 
 sudo flatpak install ${FLATPAK[@]}
-
-sudo snap install ${SNAP[@]}
